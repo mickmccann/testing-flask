405e5fd2f4febeb9030c3d239313fd048f84ea0e
mickmccann <mickmccann@ymail.com>
2021-03-27 21:09:08 +0000

    Setting up python and flask

---
+++ first.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+def print_message(message):
+    print(message)
+
+
+print_message("Hello world!")
+
+my_list = [1, 2, 3, 4, 5]
+
+for item in my_list:
+    print(item)
